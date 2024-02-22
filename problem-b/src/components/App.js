b5f016d154776c6323b3d41bd2b6edfc53425214
iefegensh <gefeis3@uw.edu>
2024-02-22 03:01:56 -0800

    part b done!

--- problem-b/src/components/App.js
+++ problem-b/src/components/App.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AboutNav } from './Navigation';
+import {BreedNav} from './Navigation';
+import _ from 'lodash';
+import PetList from './PetList'; 
 
 function App(props) {
+  const [pets, setPets] = useState(props.pets);
+
+  const adoptPet = (petName) => {
+    const updatedPets = pets.map(pet => 
+      pet.name === petName ? { ...pet, adopted: true } : pet
+    );
+    setPets(updatedPets);
+  };
+
+  const groupedByBreed = _.groupBy(props.pets, 'breed'); 
+  const uniqueBreeds = Object.keys(groupedByBreed);
+
   return (
-    <div>Replace this with your content!</div>
+    <div>
+      <header className="jumbotron jumbotron-fluid py-4">
+        <div className="container">
+          <h1>Adopt a Pet</h1>
+        </div>
+      </header>
+      
+      <main className="container">
+        <div className="row">
+          <div id="navs" className="col-3">
+            <BreedNav breeds={uniqueBreeds} />
+            <AboutNav />
+          </div> 
+          <div id="petList" className="col-9">
+            <PetList pets={pets} onAdoptPet={adoptPet} />
+          </div>
+        </div> 
+      </main>
+          
+      <footer className="container">
+        <small>Images from <a href="http://www.seattlehumane.org/adoption/dogs">Seattle Humane Society</a></small>
+      </footer>
+    </div>
   );
 }
 
--- problem-b/src/components/PetList.js
+++ problem-b/src/components/PetList.js
@@ -1,1 +1,33 @@
 import React from 'react';
+
+function PetCard({ petData, onAdoptPet }) {
+    const handleAdopt = () => {
+        onAdoptPet(petData.name);
+    };
+
+    return (
+        <div className="card" onClick={handleAdopt}>
+            <img className="card-img-top" src={petData.img} alt={petData.name} />
+            <div className="card-body">
+                <h3 className="card-title">{petData.name} {petData.adopted ? "(Adopted)" : ""}</h3>
+                <p className="card-text">{`${petData.sex} ${petData.breed}`}</p>
+                <button onClick={() => onAdoptPet(petData.name)}>Adopt</button>
+            </div>
+        </div>
+    );
+}
+
+function PetList({ pets, onAdoptPet }) {
+    return (
+        <div>
+          <h2>Dogs for Adoption</h2>
+          <div className="card-deck">
+            {pets.map(pet => (
+              <PetCard key={pet.name} petData={pet} onAdoptPet={onAdoptPet} />
+            ))}
+          </div>
+        </div>
+      );
+}
+
+export default PetList;
